e23c5b0542f285f2480948ab2ad54c2c72d45da5
Marion Huebner <mphuebner@gmail.com>
2015-05-22 14:44:55 -0500

    implements blackhawks playoff win-loss d3 experiment

--- d3_blackhawks_playoffs/javascripts/scripts.js
+++ d3_blackhawks_playoffs/javascripts/scripts.js
@@ -5,44 +5,34 @@ d3.csv("/CHI_2015_playoffs.csv", function(data) {
 
 	var view = d3.select("svg");
 
-	view.selectAll("circle")
+	var bubbles = view.selectAll("circle")
 		.data(data)
 		.enter()
 			.append('circle');
 
-	view.selectAll('circle')
+	bubbles = view.selectAll('circle')
 		.data(data)
-			.attr('r', function(d) {return d.GP*10 + 'px';})
-			.attr('cx', function(d) {return d.GP+100 + 'px';})
-			.attr('cy', function(d) {return d.GP+100 + 'px';})
+			.transition()
+			.delay(1000)
+				.attr('r', function(d) {return '20px';})
+				.attr('cx', function(d) {return d.GP*30 + 'px';})
+				.attr('cy', function(d) {return d.GP*15 + 'px';})
 
-		return svg;
+				.delay(1000)
+				.style('fill', function(d){
+					if (d.game_outcome == "W"){
+						return "red";
+					}
+					else if ((d.Opponent == "Nashville Predators") && (d.game_outcome == "L")){
+						return "blue";
+					}
+					else if ((d.Opponent == "Anaheim Ducks") && (d.game_outcome == "L")) {
+						return "orange";
+					}
+					else {
+						return "gray";
+					};
+
+		view.selectAll('circle').data(data).exit().remove();
+	});
 });
-
-
-
-
-
-function projectData(data){
-
-	var circles = [];
-
-
-
-  var bubbles = d3_body.selectAll('circle')
-       .data(data)
-       .enter()
-       .append('circle')
-
-	bubbles = d3_body.selectAll('circle')
-      .data(data)
-      .transition()
-        .duration(100)
-        .attr('r', function(d){ return d.r; })
-        .attr('cx', function(d){ return d.cx; })
-        .attr('cy', function(d){ return d.cy; })
-        .style('fill', function(d){ return d.fill; })
-        .style('opacity', function(d){ return d.opacity; });
-
-  d3_body.selectAll('circle').data(data).exit().remove();
-}
